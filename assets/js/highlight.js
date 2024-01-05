5fd2f3071cabab372c43e0d14bcbd3fa09240e60
Danila Demidovich <demidovich.daniil@gmail.com>
2024-01-05 15:00:12 +0300

    chore: added HLC highlight syntax

--- assets/js/highlight.js
+++ assets/js/highlight.js
@@ -7,6 +7,7 @@ import bash from 'highlight.js/lib/languages/bash';
 import python from 'highlight.js/lib/languages/python';
 import protobuf from 'highlight.js/lib/languages/protobuf';
 import java from 'highlight.js/lib/languages/java';
+import terraform from './terraform';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('typescript', typescript);
@@ -15,6 +16,7 @@ hljs.registerLanguage('bash', bash);
 hljs.registerLanguage('python', python);
 hljs.registerLanguage('protobuf', protobuf);
 hljs.registerLanguage('java', java);
+hljs.registerLanguage('hcl', terraform);
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('pre code:not(.language-mermaid)').forEach((block) => {
